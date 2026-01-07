b8547b3bdae9a666f8d88c6f6252c6a627f3a7af
RRV <79261047648@ya.ru>
2026-01-08 00:55:07 +0300

    Added RateLimiter via Redis

--- url_shortener/main.py
+++ url_shortener/main.py
@@ -1,9 +1,9 @@
 import asyncio
 import logging
-import sys
 from contextlib import asynccontextmanager
+from typing import Annotated
 
-from fastapi import FastAPI, Request, HTTPException, status
+from fastapi import FastAPI, Request, HTTPException, status, Depends
 from fastapi.responses import JSONResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
@@ -11,6 +11,7 @@ from fastapi.templating import Jinja2Templates
 import log_utils
 from config import BASE_DIR
 from db import engine
+from limiter import get_redis, RateLimiter, get_rate_limiter
 from models import Base
 from schemas import LinkSchema
 from service import get_slug_by_url, add_url_to_db, get_url_by_slug
@@ -23,12 +24,42 @@ log_utils.init_logging()
 async def lifespan(app: FastAPI):
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
+    redis = get_redis()
     yield
+    await redis.aclose()
 
 
 app = FastAPI(lifespan=lifespan)
 app.mount("/static", StaticFiles(directory=BASE_DIR / "static"), name="static")
 templates = Jinja2Templates(directory=BASE_DIR / "templates")
+
+
+def rate_limiter_factory(
+    endpoint: str,
+    max_requests: int,
+    window_seconds: int,
+):
+    async def dependency(
+        request: Request,
+        rate_limiter: Annotated[RateLimiter, Depends(get_rate_limiter)],
+    ):
+        ip_address = request.client.host
+        limited = await rate_limiter.is_limited(
+            ip_address,
+            endpoint,
+            max_requests,
+            window_seconds,
+        )
+        if limited:
+            raise HTTPException(
+                status_code=status.HTTP_429_TOO_MANY_REQUESTS,
+                detail="Too many requests",
+            )
+
+    return dependency
+
+
+rate_limit_sql = rate_limiter_factory("sql", 3, 5)
 
 
 @app.get("/")
@@ -39,12 +70,17 @@ async def index(request: Request):
     )
 
 
-@app.post("/", response_model=LinkSchema)
+@app.post(
+    "/",
+    response_model=LinkSchema,
+    dependencies=[Depends(rate_limit_sql)],
+)
 async def shorten_url(request: Request):
     data: dict = await request.json()
     if "url" not in data.keys():
         raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST, detail="Missing URL"
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Missing URL",
         )
     url = data["url"]
     slug: str = await get_slug_by_url(url)
@@ -52,7 +88,10 @@ async def shorten_url(request: Request):
         try:
             slug = await add_url_to_db(url)
         except ValueError as e:
-            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail=str(e),
+            )
     return JSONResponse(
         {
             "slug": slug,
@@ -66,7 +105,10 @@ async def redirect_by_slug(slug: str):
     try:
         url = await get_url_by_slug(slug)
     except ValueError as e:
-        raise HTTPException(status.HTTP_404_NOT_FOUND, str(e))
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=str(e),
+        )
     return RedirectResponse(url)
 
 
